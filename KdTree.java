1c5d7ec56f4a959b0b242ed1c2f4470bf3500cd9
Mohamed Gad <59421219+mjad218@users.noreply.github.com>
2023-03-18 00:55:38 +0200

    assessment  passed ����

--- KdTree.java
+++ KdTree.java
@@ -19,9 +19,11 @@ public class KdTree {
 
     private static class Node {
         private Point2D point;
+        private RectHV rect;
         private Node right;
         private Node parent;
 
+        private Point2D[] limits;
         private int level;
         private Node left;
 
@@ -29,6 +31,9 @@ public class KdTree {
             this.point = point;
             this.right = null;
             this.left = null;
+            this.parent = null;
+            this.rect = null;
+            this.limits = null;
         }
 
         void setRight(Node n) {
@@ -68,75 +73,99 @@ public class KdTree {
         }
 
         public Point2D[] limits() {
+            if (this.limits != null) {
+                return this.limits;
+            }
             Point2D p1 = null;
             Point2D p2 = null;
+            double xCoor = this.point().x();
+            double yCoor = this.point().y();
+            Node parent = this.parent();
+            Point2D[] parentLimits = { };
+            double parentLimits1XCoor = 0, parentLimits1YCoor = 0, parentLimits2XCoor = 0,
+                    parentLimits2YCoor = 0;
+            if (parent != null) {
+                parentLimits = parent.limits();
+                parentLimits1XCoor = parentLimits[0].x();
+                parentLimits1YCoor = parentLimits[0].y();
+                // parentLimits2XCoor = parentLimits[1].x();
+                // parentLimits2YCoor = parentLimits[1].y();
+            }
 
-            if (this.parent() == null) {
-                double x1 = this.level % 2 == 0 ? this.point().x() : 0;
-                double x2 = this.level % 2 == 0 ? this.point().x() : 1;
-                double y1 = this.level % 2 != 0 ? this.point().y() : 0;
-                double y2 = this.level % 2 != 0 ? this.point().y() : 1;
+            Node grandParent = null;
+            Point2D[] grandParentLimits = { };
+            double grandParentLimits1XCoor = 0, grandParentLimits1YCoor = 0, grandParentLimits2XCoor
+                    = 0,
+                    grandParentLimits2YCoor = 0;
+            if (parent != null && parent.parent() != null) {
+                grandParent = parent.parent();
+                grandParentLimits = grandParent.limits();
+                grandParentLimits1XCoor = grandParentLimits[0].x();
+                grandParentLimits1YCoor = grandParentLimits[0].y();
+                grandParentLimits2XCoor = grandParentLimits[1].x();
+                grandParentLimits2YCoor = grandParentLimits[1].y();
+            }
+
+            if (parent == null) {
+                double x1 = this.level % 2 == 0 ? xCoor : 0;
+                double x2 = this.level % 2 == 0 ? xCoor : 1;
+                double y1 = this.level % 2 != 0 ? yCoor : 0;
+                double y2 = this.level % 2 != 0 ? yCoor : 1;
 
                 p1 = new Point2D(x1, y1);
                 p2 = new Point2D(x2, y2);
                 Point2D[] points = { p1, p2 };
+                this.limits = points;
                 return points;
             }
 
             if (this.level % 2 == 0) {
                 // vertical line
-                Node parent = this.parent();
-                Point2D[] parentLimits = parent.limits();
-                Node grandParent = parent.parent();
                 double y1;
                 double y2;
-                y1 = parentLimits[0].y();
-                y2 = this.point().y() > y1 ? 1 : 0;
+                y1 = parentLimits1YCoor;
+                y2 = yCoor > y1 ? 1 : 0;
                 if (grandParent != null) {
-                    Point2D[] grandParentLimits = grandParent.limits();
-                    double grandPointLimitsYMin = Math.min(grandParentLimits[0].y(),
-                                                           grandParentLimits[1].y());
-                    double grandPointLimitsYMax = Math.max(grandParentLimits[0].y(),
-                                                           grandParentLimits[1].y());
-                    if (between(this.point().y(), y1, grandPointLimitsYMin)) {
+                    double grandPointLimitsYMin = Math.min(grandParentLimits1YCoor,
+                                                           grandParentLimits2YCoor);
+                    double grandPointLimitsYMax = Math.max(grandParentLimits1YCoor,
+                                                           grandParentLimits2YCoor);
+                    if (between(yCoor, y1, grandPointLimitsYMin)) {
                         y2 = grandPointLimitsYMin;
                     }
-                    else if (between(this.point().y(), y1, grandPointLimitsYMax)) {
+                    else if (between(yCoor, y1, grandPointLimitsYMax)) {
                         y2 = grandPointLimitsYMax;
-
                     }
                 }
-                double x = this.point().x();
+                double x = xCoor;
                 p1 = new Point2D(x, y1);
                 p2 = new Point2D(x, y2);
                 Point2D[] points = { p1, p2 };
+                this.limits = points;
+
                 return points;
             }
-            Node parent = this.parent();
-            Point2D[] parentLimits = parent.limits();
-            Node grandParent = parent.parent();
             double x1;
             double x2;
-            x1 = parentLimits[0].x();
-            x2 = this.point().x() > x1 ? 1 : 0;
+            x1 = parentLimits1XCoor;
+            x2 = xCoor > x1 ? 1 : 0;
             if (grandParent != null) {
-                Point2D[] grandParentLimits = grandParent.limits();
-                double grandPointLimitsXMin = Math.min(grandParentLimits[0].x(),
-                                                       grandParentLimits[1].x());
-                double grandPointLimitsXMax = Math.max(grandParentLimits[0].x(),
-                                                       grandParentLimits[1].x());
-                if (between(this.point().x(), x1, grandPointLimitsXMin)) {
+                double grandPointLimitsXMin = Math.min(grandParentLimits1XCoor,
+                                                       grandParentLimits2XCoor);
+                double grandPointLimitsXMax = Math.max(grandParentLimits1XCoor,
+                                                       grandParentLimits2XCoor);
+                if (between(xCoor, x1, grandPointLimitsXMin)) {
                     x2 = grandPointLimitsXMin;
                 }
-                else if (between(this.point().x(), x1, grandPointLimitsXMax)) {
+                else if (between(xCoor, x1, grandPointLimitsXMax)) {
                     x2 = grandPointLimitsXMax;
-
                 }
             }
-            double y = this.point().y();
+            double y = yCoor;
             p1 = new Point2D(x1, y);
             p2 = new Point2D(x2, y);
             Point2D[] points = { p1, p2 };
+            this.limits = points;
             return points;
         }
 
@@ -157,39 +186,64 @@ public class KdTree {
             Color color = this.level() % 2 == 0 ? StdDraw.RED : StdDraw.BLUE;
             StdDraw.setPenColor(color);
             StdDraw.line(p1.x(), p1.y(), p2.x(), p2.y());
+            // StdDraw.setPenColor(new Color(0, 0, 0, 80));
+            // this.rectHV().draw();
+        }
+
+        public RectHV getRect() {
+            if (this.rect == null) {
+                this.rect = this.rectHV();
+                return this.rect;
+            }
+            return this.rect;
+        }
+
+        public void setRect(RectHV r) {
+            this.rect = r;
         }
 
         public RectHV rectHV() {
+            Point2D[] limits = this.limits();
+            double limit1X = limits[0].x(), limit2X = limits[1].x();
+            double limit1Y = limits[0].y(), limit2Y = limits[1].y();
+            Point2D[] parentLimits = null;
+            double plimit1X = 0, plimit2X = 0;
+            double plimit1Y = 0, plimit2Y = 0;
+
+            if (this.parent() != null) {
+                parentLimits = this.parent().limits();
+                plimit1X = parentLimits[0].x();
+                plimit2X = parentLimits[1].x();
+                plimit1Y = parentLimits[0].y();
+                plimit2Y = parentLimits[1].y();
+
+            }
+
             if (this.level() % 2 == 0) {
                 // vertical
                 // y1 , y2
-                Point2D[] limits = this.limits();
                 double x1 = 0;
                 double x2 = 1;
 
                 if (this.parent() != null) {
-                    Point2D[] parentLimits = this.parent().limits();
-                    x1 = Math.min(parentLimits[0].x(), parentLimits[1].x());
-                    x2 = Math.max(parentLimits[0].x(), parentLimits[1].x());
+                    x1 = Math.min(plimit1X, plimit2X);
+                    x2 = Math.max(plimit1X, plimit2X);
                 }
-                double y1 = Math.min(limits[0].y(), limits[1].y());
-                double y2 = Math.max(limits[0].y(), limits[1].y());
+                double y1 = Math.min(limit1Y, limit2Y);
+                double y2 = Math.max(limit1Y, limit2Y);
 
                 return new RectHV(x1, y1, x2, y2);
 
             }
-            Point2D[] limits = this.limits();
-
             double y1 = 0;
             double y2 = 1;
 
             if (this.parent() != null) {
-                Point2D[] parentLimits = this.parent().limits();
-                y1 = Math.min(parentLimits[0].y(), parentLimits[1].y());
-                y2 = Math.max(parentLimits[0].y(), parentLimits[1].y());
+                y1 = Math.min(plimit1Y, plimit2Y);
+                y2 = Math.max(plimit1Y, plimit2Y);
             }
-            double x1 = Math.min(limits[0].x(), limits[1].x());
-            double x2 = Math.max(limits[0].x(), limits[1].x());
+            double x1 = Math.min(limit1X, limit2X);
+            double x2 = Math.max(limit1X, limit2X);
 
             return new RectHV(x1, y1, x2, y2);
         }
@@ -197,23 +251,14 @@ public class KdTree {
         public int compareTo(Node that) {
             if (this.level % 2 == 0) {
                 if (this.point.x() == that.point().x()) {
-                    if (this.point.y() == that.point().y()) {
-                        return 0;
-                    }
-                    return this.point.y() - that.point().y() > 0 ? 1 : -1;
-                    // return 0;
-                }
-                return this.point.x() - that.point().x() > 0 ? 1 : -1;
-            }
-            if (this.point.y() == that.point().y()) {
-                if (this.point.x() == that.point().x()) {
                     return 0;
                 }
-                return this.point.x() - that.point().x() > 0 ? 1 : -1;
-                // return 0;
-
+                return this.point.x() > that.point().x() ? 1 : -1;
             }
-            return this.point.y() - that.point().y() > 0 ? 1 : -1;
+            if (this.point.y() == that.point().y()) {
+                return 0;
+            }
+            return this.point.y() > that.point().y() ? 1 : -1;
         }
     }
 
@@ -249,7 +294,8 @@ public class KdTree {
             this.size++;
             return;
         }
-        if (n.compareTo(new Node(p)) < 0) {
+        if (n.compareTo(new Node(p)) <= 0) {
+            // go right
             Node node = n.right();
             if (node == null) {
                 Node nodeToBeInserted = new Node(p);
@@ -262,7 +308,7 @@ public class KdTree {
             insert(p, node);
             return;
         }
-
+        // go left
         Node node = n.left();
         if (node == null) {
             Node nodeToBeInserted = new Node(p);
@@ -290,11 +336,10 @@ public class KdTree {
         if (root.point().equals(point)) {
             return true;
         }
-
-        if (root.compareTo(new Node(point)) > 0) {
-            return contains(root.left(), point);
+        if (root.compareTo(new Node(point)) <= 0) {
+            return contains(root.right(), point);
         }
-        return contains(root.right(), point);
+        return contains(root.left(), point);
     }
     // does the set contain point p?
 
@@ -317,36 +362,22 @@ public class KdTree {
         if (rect == null) {
             throw new IllegalArgumentException();
         }
-        Queue<Point2D> points = new Queue<Point2D>();
-
-        return this.range(rect, root, points);
+        Queue<Point2D> points = new Queue<>();
+        this.range(rect, root, points);
+        return points;
     }
 
-    private Iterable<Point2D> range(RectHV rect, Node root, Queue<Point2D> points) {
+    private void range(RectHV rect, Node root, Queue<Point2D> points) {
         if (root == null) {
-            return points;
+            return;
         }
-        if (root.rectHV().intersects(rect)) {
-            if (rect.contains(root.point())) {
-                points.enqueue(root.point());
-            }
-
-            if (root.left() != null && root.left().rectHV().intersects(rect)) {
-                Point2D p = root.left().point();
-                if (rect.contains(p)) {
-                    points.enqueue(p);
-                }
-                range(rect, root.left(), points);
-            }
-            if (root.right() != null && root.right().rectHV().intersects(rect)) {
-                Point2D p = root.right().point();
-                if (rect.contains(p)) {
-                    points.enqueue(p);
-                }
-                range(rect, root.right(), points);
-            }
+        if (rect.contains(root.point())) {
+            points.enqueue(root.point());
         }
-        return points;
+        if (rect.intersects(root.getRect())) {
+            range(rect, root.left(), points);
+            range(rect, root.right(), points);
+        }
     }
 
     // all points that are inside the rectangle (or on the boundary)
@@ -355,32 +386,40 @@ public class KdTree {
         if (point == null) {
             throw new IllegalArgumentException();
         }
-        Point2D nearestPoint = new Point2D(1, 1);
-        return nearest(point, this.root, nearestPoint, 1);
+        if (this.root == null) {
+            return null;
+
+        }
+        Point2D p = this.root.point();
+        return nearest(point, this.root, p);
     }
 
-    private Point2D nearest(Point2D point, Node root, Point2D nearestPoint,
-                            double smallestDistance) {
+    private Point2D nearest(Point2D point, Node root, Point2D nearestPoint) {
         if (root == null) {
-            return null;
+            return nearestPoint;
         }
-        double distance = root.rectHV().distanceTo(point);
-        if (distance < smallestDistance) {
-            double distanceToRoot = root.point().distanceTo(point);
-            if (distanceToRoot < smallestDistance) {
-                smallestDistance = distanceToRoot;
-                nearestPoint = root.point();
-            }
+        double distance = root.getRect().distanceSquaredTo(point);
+        double smallestDistance = nearestPoint.distanceSquaredTo(point);
 
-            if (root.left() != null && root.left().rectHV().contains(point)) {
-                nearest(point, root.left(), nearestPoint, smallestDistance);
-                nearest(point, root.right(), nearestPoint, smallestDistance);
+        double distanceToRoot = root.point().distanceSquaredTo(point);
+        if (distanceToRoot < smallestDistance) {
+            nearestPoint = root.point();
+        }
+        if (distance <= smallestDistance) {
+            if (root.right() != null && root.right().getRect().contains(point)) {
+                nearestPoint = nearest(point, root.right(), nearestPoint);
+                if (root.left() != null) {
+                    nearestPoint = nearest(point, root.left(), nearestPoint);
+                }
             }
             else {
-                nearest(point, root.right(), nearestPoint, smallestDistance);
-                nearest(point, root.left(), nearestPoint, smallestDistance);
+                if (root.left() != null) {
+                    nearestPoint = nearest(point, root.left(), nearestPoint);
+                }
+                if (root.right() != null) {
+                    nearestPoint = nearest(point, root.right(), nearestPoint);
+                }
             }
-
         }
         return nearestPoint;
     }
